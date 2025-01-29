79c0a70f6657a860696662e5b934ce928ba8edc0
Am911 <amitk1261998@gmail.com>
2025-01-29 11:08:59 +0530

    xdf

--- TestWithAzure/Controllers/SampleController.cs
+++ TestWithAzure/Controllers/SampleController.cs
@@ -24,7 +24,7 @@ namespace TestWithAzure.Controllers
             db.SaveChanges();
             return RedirectToAction("Create");
         }
-        //Testing 
+        //Testing 1
         public IActionResult GetAll()
         {
             List<UserInformation> UI = db.Azure_UserInformation_Mst.ToList();
